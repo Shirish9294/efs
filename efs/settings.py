8f4ce4bb90f68833daa819bf844f0fd794eabebb
Shirish9294 <37794995+Shirish9294@users.noreply.github.com>
2021-01-31 18:38:49 -0600

    Update settings.py

--- efs/settings.py
+++ efs/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&k9)4up#-!hix++vg_d9@2*d*3q@qpen-y(_ex3eskdmbct!++'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
